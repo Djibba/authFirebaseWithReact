ca1c2f15a6e1db8bc6acc0e0f67117463e828aa7
Djibba <djibba16084@gmail.com>
2023-05-26 16:11:12 +0000

    finish signUp page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,23 @@
 import React,{useContext} from 'react';
 import {Link} from 'react-router-dom';
 import { UserContext } from '../Context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../Firebase-config';
+import { signOut } from 'firebase/auth';
 
 export default function Navbar() {
 
   const {toggle} = useContext(UserContext)
+  const navigate = useNavigate()
+  const logOut = async () => {
+    try {
+      await signOut(auth)
+      navigate('/')
+    } catch (error) {
+      alert("Error logging out")
+    }
+  }
+
 
   return (
     <nav className="navbar navbar-light bg-light px-4">
@@ -15,7 +28,7 @@ export default function Navbar() {
         <div>
           <button className='btn btn-primary' onClick={() => toggle('signup')} >Sign Up</button>
           <button className='btn btn-primary ms-2' onClick={() => toggle('signin')}>Sign In</button>
-          <button className='btn btn-danger ms-2'>Logout</button>
+          <button onClick={logOut} className='btn btn-danger ms-2'>Logout</button>
         </div>
     </nav>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from '../Context/UserContext'
 
 export default function Home() {
+
+  const {currentUser} = useContext(UserContext)
+
   return (
     <div className='container p-5'>
         <h1 className="display-3 text-light">
-            Hi, Sign Up or Sign In
+            {currentUser ? `Welcome ${currentUser.email}` : "Hi, Sign Up or Sign In"}
         </h1>
     </div>
   )
